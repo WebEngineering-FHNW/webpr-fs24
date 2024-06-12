246b5849c25fb5fbd81569e45c4c0e63b8bd66c0
Dierk Koenig <dierk.koenig@canoo.com>
2024-06-12 10:35:40 +0200

    after week 15

--- week15/crazy/crazyTest.js
+++ week15/crazy/crazyTest.js
@@ -9,9 +9,9 @@ crazySuite.add("equals", assert => {
     const b =  0;
     const c = [];
 
-    // assert.is( a == b, );
-    // assert.is( b == c, );
-    // assert.is( a == c, );
+    assert.is( a == b, true);
+    assert.is( b == c, true);
+    assert.is( a == c, false);
 });
 
 crazySuite.add("false", assert => {
@@ -30,6 +30,7 @@ crazySuite.add("coercion", assert => {
     assert.isTrue( +"2"  == 2     );
     assert.isTrue( !"0"  == false );
     assert.isTrue( !!"0" == true  );
+    assert.isTrue( Boolean("0") == true  );
     assert.is  ( Number("0"), 0 );
 });
 
@@ -37,40 +38,42 @@ crazySuite.add("object", assert => {
 
     const coerce = x => x ? true : false ;
 
-    // assert.is( coerce("0")            , );
-    // assert.is( coerce(+"0")           , );
-    // assert.is( coerce(Number("0"))    , );
-    // assert.is( coerce(new Number("0")), );
+    assert.is( coerce("0")            , true);
+    assert.is( coerce(+"0")           , false);
+    assert.is( coerce(Number("0"))    , false);
+    assert.is( coerce(new Number("0")), true);
 
 });
 
 crazySuite.add("refactor", assert => {
 
-    // const x = ; // fill here
+    const x = 0; // fill here
     //
-    // const if_1 = x => (x == true) ? true : false ;
-    // const if_2 = x =>  x          ? true : false ; // safe refactoring ???
+    const if_1 = x => (x == true) ? true : false ;
+    const if_2 = x =>  x          ? true : false ; // safe refactoring ???
     //
-    // assert.is( if_1(x), if_2(x) );  // is this true for every x ???
+    assert.is( if_1(x), if_2(x) );  // is this true for every x ???
 
 });
 
 
 crazySuite.add("other", assert => {
 
-    // assert.is( "2" + 1,      );
-    // assert.is( "2" - 1,      );
-    // assert.is( "2" - - 1,    );
-    // assert.is( 1 + 2 + "3",  );
+    assert.is( "2" + 1,      "21");
+    assert.is( "2" - 1,      1);
+    assert.is( "2" - - 1,    3);
+    assert.is( 1 + 2 + "3",  "33");
+    assert.is( 1 + 2 + Number("3"),  6);
 
-    // assert.is( +true,        );
-    // assert.is( +false,       );
-    // assert.is( true + true,  );
-    // assert.is( [] == [],     );
-    // assert.is( [] == ![],    );
-    // assert.is( +[],          );
-    // assert.is( 2 == [2],     );
-    // assert.is( [] + {},      );
+    assert.is( +true,        1);
+    assert.is( +false,       0);
+    assert.is( true + true,  2);
+    assert.is( [] == [],     false);
+    assert.is( [] == ![],    true);
+    assert.is( +[],          0);
+    assert.is( +"",          0);
+    assert.is( 2 == [2],     true);
+    assert.is( [] + {},      "[object Object]");
     // assert.is( {} + [],      );
 
 });
@@ -81,29 +84,30 @@ crazySuite.add("numbers", assert => {
     // assert.is(JSON.parse("-0") ,        );
     // assert.is(JSON.stringify(-0) ,      );
     // assert.is(String(-0) ,              );
-    // assert.is(typeof null ,             );
-    // assert.is(null instanceof Object ,  );
-    // assert.is(typeof NaN ,              );
+    assert.is(typeof null ,             "object");
+    assert.is(null instanceof Object ,  false);
+    assert.is(typeof NaN ,              "number");
     //
-    // assert.is(typeof (1/0) ,            );
-    // assert.is(0.1 + 0.2 === 0.3 ,       );
+    assert.is( (1/0) ,                Infinity);
+    assert.is(typeof (1/0) ,                "number");
+    assert.is(0.1 + 0.2 === 0.3 ,       false);
     // assert.is(999999999999999999 ,      );
     // assert.is(Number.MAX_VALUE > 0 ,    );
     // assert.is(Number.MIN_VALUE < 0 ,    );
     //
-    // assert.is(Math.min(1, 2, 3) < Math.max(1, 2, 3) ,  );
-    // assert.is(Math.min() < Math.max(),  );
+    assert.is(Math.min(1, 2, 3) < Math.max(1, 2, 3) ,  true);
+    assert.is(Math.min() <= Math.max(),  false);
 
 });
 
 crazySuite.add("compare", assert => {
 
-    // assert.is(1 < 2 < 3 ,            );
-    // assert.is(3 > 2 > 1 ,            );
+    assert.is(1 < 2 < 3 ,            true);
+    assert.is(3 > 2 > 1 ,            false);
 
-    // assert.is( {} == {} ,            );
-    // assert.is( {} >  {} ,            );
-    // assert.is( {} >= {} ,            );
+    assert.is( {} == {} ,            false);
+    assert.is( {} >  {} ,            false);
+    assert.is( {} >= {} ,            true);
 
 });
 
